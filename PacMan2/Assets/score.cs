17a2394963482daa150ae5d68148e31ccb66a831
Erica Lopes <33881813+ericaolopess@users.noreply.github.com>
2020-05-28 21:16:52 -0300

    aula 28/05

---
+++ PacMan2/Assets/score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public int pontos;
+    public GameObject coletavel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pontos = 0;
+    }
+
+    public void addPonto()
+    {
+        pontos++;
+        GetComponent<Text>().text = "Score: " + pontos;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ PacMan2/Assets/vida.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class vida : MonoBehaviour
+{
+    public GameObject player;
+    public int vidas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  vidas = player.GetComponent<Player>().vidas;
+     //   GetComponent<Text>().text = "Vida: " + vidas;
+    }
+}
